be510827e15c7f4f268bd160d0def2f07e0f691f
Eric Nation <eric.nation@gmail.com>
2014-08-15 23:48:15 -0700

    fixed gulp typo

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    coffee = require('gulp-coffee');
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/*.coffee'];
 
@@ -8,5 +9,5 @@ gulp.task('coffee', function () {
   gulp.src(coffeeSources)
     .pipe(coffee({bare: true})
       .on('error', gutil.log))
-    .pipe(gulp.dest('comoponents/scripts'))
+    .pipe(gulp.dest('components/scripts'))
 });
